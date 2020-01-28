62b5e12190558c9a5e75bc8e718addbb06217ee6
nmhbim <hoangnm.nuce59@gmai.com>
2020-01-28 13:37:11 +0700

    Updates

---
+++ precache-manifest.ad4b8fe82c6cb5e5018d0b55abc1b3b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65cf9e3c65ccae28a90a80b0cfb8bfaf",
+    "url": "/monsters-dex/index.html"
+  },
+  {
+    "revision": "a15a538465f08a752cce",
+    "url": "/monsters-dex/static/css/main.9589d5f6.chunk.css"
+  },
+  {
+    "revision": "ea99c5cf7cc1371ea188",
+    "url": "/monsters-dex/static/js/2.2d8de210.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-dex/static/js/2.2d8de210.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a15a538465f08a752cce",
+    "url": "/monsters-dex/static/js/main.a534e74e.chunk.js"
+  },
+  {
+    "revision": "5b3062c7d3fc5c9c357c",
+    "url": "/monsters-dex/static/js/runtime-main.df0460ed.js"
+  }
+]);
